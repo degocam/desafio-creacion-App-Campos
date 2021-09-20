f46462185601cc0767acfb2b253a93510850b9d9
degocam <dcamcar@gmail.com>
2021-09-20 02:00:11 -0300

    subida a repositorio previo a github

---
+++ my-app/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+console.log("Estamos Listos!");
+
+const App = () =>{
+  return(
+    <p>Bienvenido a la tienda de series y películas</p>
+  )
+}
+
+const Titulo = () =>{
+  const name = "Staggerer";
+  return(<h1>{name}</h1>)
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+    <Titulo />
+    <App />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
